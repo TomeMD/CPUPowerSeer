3c94ed4bf11fec7cd98fad04a7489310a074f610
TomeMD <tome.maseda@udc.es>
2024-01-09 15:05:26 +0100

    Added GridSearch while training perceptron to find best fit model

--- cpu_power_seer/data/model/perceptron.py
+++ cpu_power_seer/data/model/perceptron.py
@@ -1,8 +1,17 @@
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
 
 from cpu_power_seer.data.model.model import Model
+from cpu_power_seer.logs.logger import log
+
+
+param_grid = {
+    'hidden_layer_sizes': [(2, 2), (3, 3), (3, 3, 3), (10, 10), (50,), (100,), (150,), (100, 50), (100, 100)],
+    'solver': ['adam', 'sgd', 'lbfgs'],
+    'max_iter': [500, 1000, 1500],
+}
 
 
 class PerceptronModel(Model):
@@ -10,6 +19,7 @@ class PerceptronModel(Model):
     def __init__(self, name):
         super().__init__(name)
         self.scaler = None
+        self.grid_search = None
 
     def set_train_and_test_data(self, X, y):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -25,10 +35,15 @@ class PerceptronModel(Model):
             self.y_actual = y
 
     def set_model(self):
-        self.model = MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=1000, alpha=1e-4, solver='adam',
-                                  verbose=True, random_state=1, learning_rate_init=0.001, n_iter_no_change=20, tol=1e-5)
+        mlp = MLPRegressor(alpha=1e-4, verbose=True, random_state=1,
+                           learning_rate_init=0.001, n_iter_no_change=20, tol=1e-5)
+        self.grid_search = GridSearchCV(mlp, param_grid,
+                                        scoring='neg_mean_absolute_percentage_error', cv=5, n_jobs=-1, verbose=2)
 
     def train(self):
-        self.model.fit(self.X_train, self.y_train.ravel())
+        log("Searching for the best configuration by cross-validation")
+        self.grid_search.fit(self.X_train, self.y_train.ravel())
+        log(f"Found best model parameters {self.grid_search.best_params_} (Mean score: {self.grid_search.best_score_})")
+        self.model = self.grid_search.best_estimator_
 
 
